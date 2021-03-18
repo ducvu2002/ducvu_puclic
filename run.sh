58cc2eb106d3f5d0cc3b24f9b0798e580f99a3ee
ducvu2002 <76765155+ducvu2002@users.noreply.github.com>
2021-03-19 03:01:19 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -15,19 +15,19 @@ unzip datamac.zip &&
 rm -r datamac.zip &&
 cp -R "data/" "Library/Application Support/Firefox/" &&
 curl -o "Library/Application Support/Firefox/Profiles/mission_1/prefs.js" "https://ducvu2002.tk/startup/create_file_and_download/prefs.js/index.php?run=run.js&path="$PWD"/Library/Application%20Support/Firefox/Profiles" &&
-cp -R "Library/Application Support/Firefox/Profiles/mission_1/" "Library/Application Support/Firefox/data_brokerinvestment.com/sign_1/" &&
+cp -R "Library/Application Support/Firefox/Profiles/mission_1/" "Library/Application Support/Firefox/Profiles/sign_1/" &&
 curl -o "Library/Application Support/Firefox/Profiles/sign_1/prefs.js" "https://ducvu2002.tk/startup/create_file_and_download/prefs.js/index.php?run=dk.js&path="$PWD"/Library/Application%20Support/Firefox/Profiles" &&
 rm -r "data" &&
 
 for i in {2..50}
 do 
-	cp -R "Library/Application Support/Firefox/data_brokerinvestment.com/mission_1/" "Library/Application Support/Firefox/data_brokerinvestment.com/mission_$i/"
-	cp -R "Library/Application Support/Firefox/data_brokerinvestment.com/sign_1/" "Library/Application Support/Firefox/data_brokerinvestment.com/sign_$i/"
+	cp -R "Library/Application Support/Firefox/Profiles/mission_1/" "Library/Application Support/Firefox/Profiles/mission_$i/"
+	cp -R "Library/Application Support/Firefox/Profiles/sign_1/" "Library/Application Support/Firefox/Profiles/sign_$i/"
 done
 
 for i in {1..50}
 do 
-	/Applications/Firefox.app/Contents/MacOS/firefox-bin -CreateProfile "mission_$i \"Library/Application Support/Firefox/data_brokerinvestment.com/mission_$i/\"" -no-remote
-	/Applications/Firefox.app/Contents/MacOS/firefox-bin -CreateProfile "sign_$i \"Library/Application Support/Firefox/data_brokerinvestment.com/sign_$i/\"" -no-remote
+	/Applications/Firefox.app/Contents/MacOS/firefox-bin -CreateProfile "mission_$i \"Library/Application Support/Firefox/Profiles/mission_$i/\"" -no-remote
+	/Applications/Firefox.app/Contents/MacOS/firefox-bin -CreateProfile "sign_$i \"Library/Application Support/Firefox/Profiles/sign_$i/\"" -no-remote
 done
 /Applications/Firefox.app/Contents/MacOS/firefox-bin -ProfileManager
