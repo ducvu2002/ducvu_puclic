ae71ed1405b03aed874adec479d36f3233970fff
ducvu2002 <76765155+ducvu2002@users.noreply.github.com>
2021-08-15 16:16:41 +0700

    Update kh6_3.js

--- bantool/kh6_3.js
+++ bantool/kh6_3.js
@@ -6,16 +6,16 @@ j2 = -1;
 var j2;
 
 function isNumeric(str) {
-  if (typeof str != "string") return false // we only process strings!  
-  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+	if (typeof str != "string") return false // we only process strings!  
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+	!isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
 var so_luong;
 while (true) {
 	so_luong = prompt('Nhập số luồng');
-    if (isNumeric(so_luong)) break;
-    alert('Nhập không đúng vui lòng nhập lại');
+	if (isNumeric(so_luong)) break;
+	alert('Nhập không đúng vui lòng nhập lại');
 }
 
 
