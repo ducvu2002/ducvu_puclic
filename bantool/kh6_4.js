ec9422a8636516d2e83b695214b22a14b7bc7a24
ducvu2002 <76765155+ducvu2002@users.noreply.github.com>
2021-08-17 19:48:11 +0700

    Update kh6_4.js

--- bantool/kh6_4.js
+++ bantool/kh6_4.js
@@ -7,9 +7,9 @@ var x2 = -1;
 var y2 = -1;
 
 function isNumeric(str) {
-  if (typeof str != "string") return false // we only process strings!  
-  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+	if (typeof str != "string") return false // we only process strings!  
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+	 !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
 
 while (true) {
@@ -79,7 +79,7 @@ function get_csv(line, col) {
 
 
 function write_data(path, text) {
-    path = path.replace(/ /g, '<SP>');
+	path = path.replace(/ /g, '<SP>');
 	iimSet('text', text);
 	iimPlayCode(
 		'SET !EXTRACT {{text}}' + "\n" +
