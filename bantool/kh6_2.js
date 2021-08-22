890c5815458a3b461ad24716cd8bb387cfd2cf2e
ducvu2002 <76765155+ducvu2002@users.noreply.github.com>
2021-08-22 12:42:03 +0700

    Update kh6_2.js

--- bantool/kh6_2.js
+++ bantool/kh6_2.js
@@ -14,7 +14,9 @@ function get_csv(line, col) {
 }
 
 
+
 function write_data(path, text) {
+    path = path.replace(/ /g, '<SP>');
 	iimSet('text', text);
 	iimPlayCode(
 		'SET !EXTRACT {{text}}' + "\n" +
@@ -25,7 +27,9 @@ function write_data(path, text) {
 
 
 function downloadURI(url, path, name) {
-
+	url = url.replace(/ /g, '<SP>');
+	path = path.replace(/ /g, '<SP>');
+	name = name.replace(/ /g, '<SP>');
 	iimPlayCode('SET !TIMEOUT_PAGE 1' + "\n" + 'URL GOTO=about:newtab');
 	iimPlayCode('SET !TIMEOUT_PAGE 2592000' + "\n" + 'URL GOTO=' + url);
 	iimPlayCode("ONDOWNLOAD FOLDER=" + path + " FILE=" + name + " WAIT=NO");
